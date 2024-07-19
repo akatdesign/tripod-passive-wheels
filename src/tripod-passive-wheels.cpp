da642b88da3eac05ceb9436641a3fbc08479cf53
kamaaki <akihiro16910@gmail.com>
2024-07-19 14:44:17 +0900

    refactor: function name and add LED color change

--- src/tripod-passive-wheels.cpp
+++ src/tripod-passive-wheels.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
-#include <PS4Controller.h>
 #include <IcsHardSerialClass.h>
+#include <PS4Controller.h>
 
 const byte EN_PIN = 2;
 const long BAUDRATE = 115200;
-const int TIMEOUT = 10;    //通信できてないか確認用にわざと遅めに設定
+const int TIMEOUT = 10; // 通信できてないか確認用にわざと遅めに設定
 
-IcsHardSerialClass krs(&Serial, EN_PIN, BAUDRATE, TIMEOUT); //インスタンス＋ENピン(2番ピン)およびUARTの指定
+IcsHardSerialClass
+    krs(&Serial, EN_PIN, BAUDRATE,
+        TIMEOUT); // インスタンス＋ENピン(2番ピン)およびUARTの指定
 
-enum Mode {
-  front_leg,
-  right_leg,
-  left_leg
-};
+enum Mode { front_leg, right_leg, left_leg };
 
 Mode ChangeMode(Mode current) {
   if (current == front_leg) {
@@ -26,7 +24,7 @@ Mode ChangeMode(Mode current) {
   }
 }
 
-void reset_position() {
+void resetPosition() {
   for (int id = 0; id <= 24; id += 10) {
     krs.setPos(id, 7500);
     krs.setPos(id + 1, 7500 - 1500);
@@ -45,10 +43,10 @@ void forward(int right_leg_id, int left_leg_id) {
   delay(500);
 }
 
-void right_rotation() {
+void rightRotation() {
   krs.setPos(4, 7500 - 1250);
-  krs.setPos(14, 7500 - 1250 );
-  krs.setPos(24, 7500 - 1250 );
+  krs.setPos(14, 7500 - 1250);
+  krs.setPos(24, 7500 - 1250);
   delay(500);
   krs.setPos(2, 7500);
   krs.setPos(12, 7500);
@@ -64,13 +62,13 @@ void right_rotation() {
   krs.setPos(2, 7500 - 1500);
   krs.setPos(12, 7500 - 1500);
   krs.setPos(22, 7500 - 1500);
-  krs.setPos(3, 7500 - 2500 );
-  krs.setPos(13, 7500 - 2500 );
+  krs.setPos(3, 7500 - 2500);
+  krs.setPos(13, 7500 - 2500);
   krs.setPos(23, 7500 - 2500);
   delay(500);
 }
 
-void left_rotation() {
+void leftRotation() {
   krs.setPos(4, 7500 + 1750);
   krs.setPos(14, 7500 + 1750);
   krs.setPos(24, 7500 + 1750);
@@ -83,14 +81,14 @@ void left_rotation() {
   krs.setPos(23, 7500 - 1000);
   delay(500);
   krs.setPos(4, 7500 - 1250);
-  krs.setPos(14, 7500 - 1250 );
-  krs.setPos(24, 7500 - 1250 );
+  krs.setPos(14, 7500 - 1250);
+  krs.setPos(24, 7500 - 1250);
   delay(500);
   krs.setPos(2, 7500 - 1500);
   krs.setPos(12, 7500 - 1500);
   krs.setPos(22, 7500 - 1500);
-  krs.setPos(3, 7500 - 2500 );
-  krs.setPos(13, 7500 - 2500 );
+  krs.setPos(3, 7500 - 2500);
+  krs.setPos(13, 7500 - 2500);
   krs.setPos(23, 7500 - 2500);
   delay(500);
 }
@@ -98,71 +96,74 @@ void left_rotation() {
 void setup() {
   Serial.begin(115200);
   PS4.begin("08:B6:1F:ED:4B:E2");
-  krs.begin();  //サーボモータの通信初期設定
-  reset_position(); // サーボの初期位置設定
+  krs.begin();     // サーボモータの通信初期設定
+  resetPosition(); // サーボの初期位置設定
 }
 
 void loop() {
   Mode mode = front_leg;
 
-  if (PS4.isConnected()) {
-    if (PS4.Share()) {
-      mode = ChangeMode(mode);
-      Serial.print(F("\r\noptions"));
+  while (true) {
+    if (PS4.isConnected()) {
+      if (PS4.Share()) {
+        mode = ChangeMode(mode);
+        resetPosition();
+      }
+      if (mode == front_leg) {
+        PS4.setLed(255, 0, 0);
+        if (PS4.Up()) {
+          forward(0, 10);
+        }
+        if (PS4.Right()) {
+          krs.setPos(24, 7500 + 250 - 500);
+        }
+        if (PS4.Down()) {
+          krs.setPos(24, 7500 + 250);
+        }
+        if (PS4.Left()) {
+          krs.setPos(24, 7500 + 250 + 500);
+        }
+      } else if (mode == right_leg) {
+        PS4.setLed(0, 255, 0);
+        if (PS4.Up()) {
+          forward(20, 0);
+        }
+        if (PS4.Right()) {
+          krs.setPos(14, 7500 + 250 - 500);
+        }
+        if (PS4.Down()) {
+          krs.setPos(14, 7500 + 250);
+        }
+        if (PS4.Left()) {
+          krs.setPos(14, 7500 + 250 + 500);
+        }
+      } else if (mode == left_leg) {
+        PS4.setLed(0, 0, 255);
+        if (PS4.Up()) {
+          forward(10, 20);
+        }
+        if (PS4.Right()) {
+          krs.setPos(4, 7500 + 250 - 500);
+        }
+        if (PS4.Down()) {
+          krs.setPos(4, 7500 + 250);
+        }
+        if (PS4.Left()) {
+          krs.setPos(4, 7500 + 250 + 500);
+        }
+      }
+      if (PS4.Cross()) {
+        resetPosition();
+      }
+      if (PS4.L1()) {
+        leftRotation();
+        resetPosition();
+      }
+      if (PS4.R1()) {
+        rightRotation();
+        resetPosition();
+      }
+      PS4.sendToController();
     }
-    if (mode == front_leg) {
-      if (PS4.Up()) {
-        Serial.print(("\r\nup"));
-        forward(0, 10);
-      }
-      if (PS4.Right()) {
-        krs.setPos(24, 7500 + 250 -500);
-      }
-      if (PS4.Down()) {
-        krs.setPos(24, 7500 + 250);
-      }
-      if (PS4.Left()) {
-        krs.setPos(24, 7500 + 250 + 500);
-      }
-    } else if (mode == right_leg) {
-      if (PS4.Up()) {
-        forward(20, 0);
-        Serial.print(("\r\nright_up"));
-      }
-      if (PS4.Right()) {
-        krs.setPos(14, 7500 + 250 - 500);
-      }
-      if (PS4.Down()) {
-        krs.setPos(14, 7500 + 250);
-      }
-      if (PS4.Left()) {
-        krs.setPos(14, 7500 + 250 + 500);
-      }
-    } else if (mode == left_leg) {
-      if (PS4.Up()) {
-        forward(10, 20);
-      }
-      if (PS4.Right()) {
-        krs.setPos(4, 7500 + 250 - 500);
-      }
-      if (PS4.Down()) {
-        krs.setPos(4, 7500 + 250);
-      }
-      if (PS4.Left()) {
-        krs.setPos(4, 7500 + 250 + 500);
-      }
-    }
-    if (PS4.Cross()) {
-      reset_position();
-    }
-    if (PS4.L1()) {
-      left_rotation();
-    }
-    if (PS4.R1()) {
-      right_rotation();
-    }
-
   }
 }
-
-// Serial.print(F("\r\nLeft"));
